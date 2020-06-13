7f40369fd8dc7b4beb01da5d62553a2e2c20946c
Iana Kain <ianakain@iana.kain>
2020-06-13 20:27:18 +0300

    templates refactor, typing event bugfix, removed rooms db

--- db/index.js
+++ db/index.js
@@ -1,9 +1,7 @@
 const Users = require('./users');
-const Rooms = require('./rooms');
 const Chat = require('./chat');
 
 exports.collections = () => ({
   users: new Users(),
-  rooms: new Rooms(),
   chat: new Chat(),
 });
